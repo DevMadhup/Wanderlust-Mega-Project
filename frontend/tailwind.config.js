3d236312a91fff754cbe75484b77eed4655642ee
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2026-04-24 22:37:49 +0530

    fix(project-setup): resolve module issues caused by ESM config and alias misconfiguration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme');
+import colors from 'tailwindcss/colors'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
+// @type {import('tailwindcss').Config}
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
@@ -38,4 +38,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
